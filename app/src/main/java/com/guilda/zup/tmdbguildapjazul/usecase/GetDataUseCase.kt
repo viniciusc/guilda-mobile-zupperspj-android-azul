3ef5594b6773f791dbb43b0974048ddcce0587b9
everton4292 <ecoresende@hotmail.com>
2024-05-27 18:24:58 -0300

    Criação de uistate, view model factory, usecase sample

---
+++ app/src/main/java/com/guilda/zup/tmdbguildapjazul/usecase/GetDataUseCase.kt
@@ -0,0 +1,18 @@
+package com.guilda.zup.tmdbguildapjazul.usecase
+
+import com.guilda.zup.tmdbguildapjazul.model.UiState
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.onStart
+
+class GetDataUseCase {
+    //TODO: create repository
+    fun getData(): Flow<UiState<String>> {
+        return flow {
+            emit(UiState.Loading)
+            emit(UiState.Success("UserName"))
+        }.onStart { emit(UiState.Loading) }
+            .catch { e -> emit(UiState.Error("Fatal error${e.cause}")) }
+    }
+}
---
+++ app/src/main/java/com/guilda/zup/tmdbguildapjazul/viewmodel/MainViewModel.kt
@@ -0,0 +1,25 @@
+package com.guilda.zup.tmdbguildapjazul.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.guilda.zup.tmdbguildapjazul.model.UiState
+import com.guilda.zup.tmdbguildapjazul.usecase.GetDataUseCase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class MainViewModel(private val getDataUseCase: GetDataUseCase) : ViewModel() {
+
+    private val _uiStateLiveData = MutableStateFlow<UiState<String>>(UiState.Loading)
+
+    val uiStateLiveData: StateFlow<UiState<String>> = _uiStateLiveData
+
+    init {
+        viewModelScope.launch {
+
+            getDataUseCase.getData().collect { uiState ->
+                _uiStateLiveData.value = uiState
+            }
+        }
+    }
+}
