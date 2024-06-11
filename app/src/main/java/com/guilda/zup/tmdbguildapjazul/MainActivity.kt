ff8f7f7d170de1acef4804673de4d0aca220a419
Vinicius <fieldy2@gmail.com>
2024-06-11 09:07:40 -0300

    ajuste merge

--- app/src/main/java/com/guilda/zup/tmdbguildapjazul/MainActivity.kt
+++ app/src/main/java/com/guilda/zup/tmdbguildapjazul/MainActivity.kt
@@ -7,18 +7,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import com.guilda.zup.tmdbguildapjazul.theme.ComposeByExampleTheme
 import com.guilda.zup.tmdbguildapjazul.ui.Home
-import androidx.compose.ui.tooling.preview.Preview
 import com.guilda.zup.tmdbguildapjazul.model.UiState
-import com.guilda.zup.tmdbguildapjazul.ui.theme.TMDBGuildaPJAzulTheme
 
 class MainActivity : ComponentActivity() {
 
