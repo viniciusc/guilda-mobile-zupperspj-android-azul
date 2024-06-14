a52c07c1bf16f9d15b66e0a5aae6eb9a7336cbe7
jcastro <josediogonc@gmail.com>
2024-06-14 16:59:31 -0300

    ajustes

--- app/src/main/java/com/guilda/zup/tmdbguildapjazul/data/repository/api/ApiService.kt
+++ app/src/main/java/com/guilda/zup/tmdbguildapjazul/data/repository/api/ApiService.kt
@@ -3,15 +3,10 @@ package com.guilda.zup.tmdbguildapjazul.data.repository.api
 import com.guilda.zup.tmdbguildapjazul.data.repository.model.MovieSearchResponse
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Header
-import retrofit2.http.Path
 import retrofit2.http.Query
 
-
-
-
-
 interface ApiService {
+
     @GET("search/movie")
     suspend fun searchMovie(
         @Query("query") query: String,
