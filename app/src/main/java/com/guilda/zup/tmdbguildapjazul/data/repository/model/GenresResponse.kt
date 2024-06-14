bb5a1e1ebd922754c0924d94d0fbc28be2d84543
jcastro <josediogonc@gmail.com>
2024-06-14 18:05:31 -0300

    ajustes no modelo

--- app/src/main/java/com/guilda/zup/tmdbguildapjazul/data/repository/model/GenresResponse.kt
+++ app/src/main/java/com/guilda/zup/tmdbguildapjazul/data/repository/model/GenresResponse.kt
@@ -3,4 +3,6 @@ package com.guilda.zup.tmdbguildapjazul.data.repository.model
 data class GenresResponse(
     val genres: List<Genre>
 )
-fun GenresResponse.genresMap() = this.genres.associateBy({ it.id }, { it.name })
+
+typealias GenresMap = Map<Int, String>
+fun GenresResponse.genresMap(): GenresMap = this.genres.associateBy({ it.id }, { it.name })
--- app/src/main/java/com/guilda/zup/tmdbguildapjazul/data/repository/model/Movie.kt
+++ app/src/main/java/com/guilda/zup/tmdbguildapjazul/data/repository/model/Movie.kt
@@ -1,18 +1,23 @@
 package com.guilda.zup.tmdbguildapjazul.data.repository.model
 
+import com.google.gson.annotations.SerializedName
+import com.squareup.moshi.Json
+
 data class Movie(
-    val adult: Boolean,
-    val backdropPath: String?,
-    val genreIds: List<Double>,
-    val id: Double,
-    val originalLanguage: String,
-    val originalTitle: String,
-    val overview: String,
-    val popularity: Double,
-    val posterPath: String?,
-    val releaseDate: String,
-    val title: String,
-    val video: Boolean,
-    val voteAverage: Double,
-    val voteCount: Double
+    @field:Json(name = "adult") val adult: Boolean,
+    @field:Json(name = "backdrop_path") val backdropPath: String?,
+    @field:Json(name = "genre_ids") val genreIds: List<Int>,
+    @field:Json(name = "id") val id: Int,
+    @field:Json(name = "original_language") val originalLanguage: String,
+    @field:Json(name = "original_title") val originalTitle: String,
+    @field:Json(name = "overview") val overview: String,
+    @field:Json(name = "popularity") val popularity: Double,
+    @field:Json(name = "poster_path") val posterPath: String?,
+    @field:Json(name = "release_date") val releaseDate: String,
+    @field:Json(name = "title") val title: String,
+    @field:Json(name = "video") val video: Boolean,
+    @field:Json(name = "vote_average") val voteAverage: Double,
+    @field:Json(name = "vote_count") val voteCount: Double
 )
+
+fun Movie.getGenres(genresMap: GenresMap) = genreIds.mapNotNull { genresMap[it.toInt()] }
